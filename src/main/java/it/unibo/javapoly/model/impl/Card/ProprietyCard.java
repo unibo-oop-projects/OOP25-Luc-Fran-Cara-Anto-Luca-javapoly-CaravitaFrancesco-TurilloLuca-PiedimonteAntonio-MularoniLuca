1616990ef8f2c468d03e26882554f23e804667c5
Francesco Caravita <francesco.caravita2@studio.unibo.it>
2026-01-30 16:58:46 +0100

    added new CDC

--- src/main/java/it/unibo/javapoly/model/impl/Card/ProprietyCard.java
+++ src/main/java/it/unibo/javapoly/model/impl/Card/ProprietyCard.java
@@ -1,25 +1,66 @@
 package it.unibo.javapoly.model.impl.Card;
 
+import java.util.List;
+
 import it.unibo.javapoly.model.api.card.Card;
 
 public class ProprietyCard implements Card{
 
+    final String id;
+    final String name;
+    final String description;
+
+    final String color;
+
+    final int baseRent;
+    final List<Integer> multiProroprietyCost;
+    final int hotelRent;
+    final int proprietyCost;
+
+    final int houseCost;
+    final int hotelCost;
+
+
+    public ProprietyCard(final String id, final String name, final String description, 
+        final String color, final int baseRent, final List<Integer> multiProroprietyCost, final int hotelRent, 
+        final int proprietyCost, final int houseCost, final int hotelCost) {
+        
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.color = color;
+        this.baseRent = baseRent;
+        this.multiProroprietyCost = multiProroprietyCost;
+        this.hotelRent = hotelRent;
+        this.proprietyCost = proprietyCost;
+        this.houseCost = houseCost;
+        this.hotelCost = hotelCost;
+    }
+
     @Override
     public String getId() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getId'");
+        return this.id;
     }
 
     @Override
     public String getName() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+        return this.name;
     }
 
     @Override
     public String getDescription() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDescription'");
+        return this.description;
+    }
+
+    
+
+    @Override
+    public String toString() {
+        return "ProprietyCard{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
     }
     
 }
