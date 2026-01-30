e3b074dea9e172005bae8aa0438f8e7ae429c3e4
Francesco Caravita <francesco.caravita2@studio.unibo.it>
2026-01-30 16:26:42 +0100

    added new type of card (GameCard interface)

---
+++ src/main/java/it/unibo/javapoly/model/api/card/GameCard.java
@@ -0,0 +1,6 @@
+package it.unibo.javapoly.model.api.card;
+
+public interface GameCard extends Card {
+    // TODO: definire meglio il metodo apply in base alle esigenze del gioco aggiungendo parametri 
+    void apply();
+}
