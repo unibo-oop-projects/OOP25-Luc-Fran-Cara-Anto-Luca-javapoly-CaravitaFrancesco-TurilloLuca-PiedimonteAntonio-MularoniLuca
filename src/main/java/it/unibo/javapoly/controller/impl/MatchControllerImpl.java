37f6ee506e6db857325842de2195fe6dccd8b2dd
Antonio Piedimonte <Antonio.Piedimonte@studio.unibo.it>
2026-01-31 15:52:35 +0100

    Add MatchController and MatchControllerImpl, only method stubs

---
+++ src/main/java/it/unibo/javapoly/controller/impl/MatchControllerImpl.java
@@ -0,0 +1,50 @@
+package it.unibo.javapoly.controller.impl;
+
+import it.unibo.javapoly.controller.api.MatchController;
+import it.unibo.javapoly.model.api.Player;
+
+public class MatchControllerImpl implements MatchController{
+
+    @Override
+    public void startGame() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'startGame'");
+    }
+
+    @Override
+    public void nextTurn() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'nextTurn'");
+    }
+
+    @Override
+    public Player getCurrentPlayer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCurrentPlayer'");
+    }
+
+    @Override
+    public void handleDiceThrow() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleDiceThrow'");
+    }
+
+    @Override
+    public void handleMove(int steps) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleMove'");
+    }
+
+    @Override
+    public void handlePrison() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handlePrison'");
+    }
+
+    @Override
+    public void handlePropertyLanding() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handlePropertyLanding'");
+    }
+    
+}
