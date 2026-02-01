301854b51274505a879fa1184871d5bbb6bc8959
Caravita Francesco <francesco.caravita2@studio.unibo.it>
2026-02-01 19:55:49 +0100

    Added folder utils with JsonUtils

---
+++ src/main/java/it/unibo/javapoly/utils/JsonUtils.java
@@ -0,0 +1,36 @@
+package it.unibo.javapoly.utils;
+
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+// TODO: add all the JavaDoc comment
+
+public final class JsonUtils {
+    private static final ObjectMapper MAPPER = create();
+
+    private JsonUtils() {}
+
+    private static ObjectMapper create() {
+        ObjectMapper m = new ObjectMapper();
+        m.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        m.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        m.enable(SerializationFeature.INDENT_OUTPUT); // per toString leggibile
+        m.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+
+        // These two lines enable the @JsonRootName of the classes.
+        m.enable(SerializationFeature.WRAP_ROOT_VALUE);
+        m.enable(DeserializationFeature.UNWRAP_ROOT_VALUE);
+        return m;
+    }
+
+    /**
+     * This method return the mapper of Jackson library. 
+     * in this way every classes use the same object. 
+     * This ObjectMapper its used for Serialization and deserialization of json item.
+     */
+    public static ObjectMapper mapper() {
+        return MAPPER.copy();
+    }
+}
