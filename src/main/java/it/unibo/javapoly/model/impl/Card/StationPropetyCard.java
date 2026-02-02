b48b0ff3bf9aebac06be363e4716e992f38bb085
Caravita Francesco <francesco.caravita2@studio.unibo.it>
2026-02-02 12:35:35 +0100

    Added StationPropertyCard

---
+++ src/main/java/it/unibo/javapoly/model/impl/Card/StationPropetyCard.java
@@ -0,0 +1,11 @@
+package it.unibo.javapoly.model.impl.card;
+
+public class StationPropetyCard extends AbstractProprietyCard{
+
+    @Override
+    public int calculateRent(int number) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calculateRent'");
+    }
+    
+}
