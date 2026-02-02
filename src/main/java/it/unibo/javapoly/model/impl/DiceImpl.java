f5dcece222b5a7e420ef7a79ffb6c8102ad58ac0
Luca Turillo <luca.turillo@studio.unibo.it>
2026-02-02 19:56:35 +0100

    Fix formatting and spacing in DiceImpl class

--- src/main/java/it/unibo/javapoly/model/impl/DiceImpl.java
+++ src/main/java/it/unibo/javapoly/model/impl/DiceImpl.java
@@ -4,17 +4,17 @@ import java.util.Random;
 
 import it.unibo.javapoly.model.api.Dice;
 
-public class DiceImpl implements Dice{
+public class DiceImpl implements Dice {
     private int randResult;
     private final Random rand = new Random();
 
     @Override
     public void throwDice() {
-        randResult = rand.nextInt(6) + 1; 
+        randResult = rand.nextInt(6) + 1;
     }
 
     @Override
-    public int getDicesResult(){
+    public int getDicesResult() {
         return randResult;
     }
 }
