47a228abe7d6b888573bf4d9ff02802e625ba016
Francesco Caravita <francesco.caravita2@studio.unibo.it>
2026-02-04 18:52:43 +0100

    added boardImpl Interface

---
+++ src/main/java/it/unibo/javapoly/model/impl/board/BoardImpl.java
@@ -0,0 +1,26 @@
+package it.unibo.javapoly.model.impl.board;
+
+import it.unibo.javapoly.model.api.board.Board;
+import it.unibo.javapoly.model.api.board.Tile;
+
+public class BoardImpl implements Board{
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
+    @Override
+    public Tile getTileAt(int position) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTileAt'");
+    }
+
+    @Override
+    public int normalizePosition(int position) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'normalizePosition'");
+    }
+    
+}
