ac0f691f48a06af5440ab096b46cb04bacf47909
Francesco Caravita <francesco.caravita2@studio.unibo.it>
2026-02-04 19:00:41 +0100

    add method

--- src/main/java/it/unibo/javapoly/model/impl/board/tile/AbstractTile.java
+++ src/main/java/it/unibo/javapoly/model/impl/board/tile/AbstractTile.java
@@ -1,7 +1,31 @@
 package it.unibo.javapoly.model.impl.board.tile;
 
 import it.unibo.javapoly.model.api.board.Tile;
+import it.unibo.javapoly.model.api.board.TileType;
 
 public abstract class AbstractTile implements Tile{
-    
+    private final int position;
+    private final TileType type;
+    private final String name;
+
+    protected AbstractTile(final int position, final TileType type, final String name) {
+        this.position = position;
+        this.type = type;
+        this.name = name;
+    }
+
+    @Override
+    public int getPosition() {
+        return position;
+    }
+
+    @Override
+    public TileType getType() {
+        return type;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
 }
