79ee6f57f172416e0a45f16ca9f1a6067e0be75c
Francesco Caravita <francesco.caravita2@studio.unibo.it>
2026-02-04 17:43:07 +0100

    Added method

--- src/main/java/it/unibo/javapoly/model/api/board/Tile.java
+++ src/main/java/it/unibo/javapoly/model/api/board/Tile.java
@@ -1,5 +1,14 @@
 package it.unibo.javapoly.model.api.board;
 
+/**
+ * Represents a tile on the board.
+ */
 public interface Tile {
+
+    int getPosition();
+
+    TileType getType();
     
+    String getName();
 }
+
