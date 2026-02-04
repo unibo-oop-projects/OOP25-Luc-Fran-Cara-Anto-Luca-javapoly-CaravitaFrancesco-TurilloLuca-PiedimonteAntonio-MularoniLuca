4c4452338d56e6c3c60e0831e26567ae9dfc6624
Francesco Caravita <francesco.caravita2@studio.unibo.it>
2026-02-04 18:31:57 +0100

    fixed checkstyle

--- src/main/java/it/unibo/javapoly/model/api/board/Tile.java
+++ src/main/java/it/unibo/javapoly/model/api/board/Tile.java
@@ -5,10 +5,24 @@ package it.unibo.javapoly.model.api.board;
  */
 public interface Tile {
 
+    /**
+     * Returns the position of the tile on the board.
+     *
+     * @return the tile position
+     */
     int getPosition();
 
+    /**
+     * Returns the type of the tile.
+     *
+     * @return the tile type
+     */
     TileType getType();
-    
+
+    /**
+     * Returns the name of the tile.
+     *
+     * @return the tile name
+     */
     String getName();
 }
-
