16e9273acfbf192e0e6a4b96f72766923420252f
Francesco Caravita <francesco.caravita2@studio.unibo.it>
2026-02-05 16:48:24 +0100

    added implementation of CardDeck

---
+++ src/main/java/it/unibo/javapoly/model/impl/Card/CardDeckImpl.java
@@ -0,0 +1,32 @@
+package it.unibo.javapoly.model.impl.card;
+
+import it.unibo.javapoly.model.api.card.CardDeck;
+import it.unibo.javapoly.model.api.card.GameCard;
+
+public class CardDeckImpl implements CardDeck{
+
+    @Override
+    public GameCard draw(String playerId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+    }
+
+    @Override
+    public void discard(GameCard card) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'discard'");
+    }
+
+    @Override
+    public void shuffle() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'shuffle'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+    
+}
