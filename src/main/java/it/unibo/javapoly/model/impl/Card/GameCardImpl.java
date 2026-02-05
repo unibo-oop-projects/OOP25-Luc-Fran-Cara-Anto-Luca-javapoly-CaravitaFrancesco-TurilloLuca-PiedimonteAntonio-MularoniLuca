b42590e9fc0e1bf47ae8ba37330fe7409dd3fdaf
Francesco Caravita <francesco.caravita2@studio.unibo.it>
2026-02-05 12:04:08 +0100

    added Implementation of GameCard Interface

---
+++ src/main/java/it/unibo/javapoly/model/impl/Card/GameCardImpl.java
@@ -0,0 +1,46 @@
+package it.unibo.javapoly.model.impl.card;
+
+import java.util.Map;
+
+import it.unibo.javapoly.model.api.card.CardType;
+import it.unibo.javapoly.model.api.card.GameCard;
+
+public class GameCardImpl implements GameCard{
+
+    @Override
+    public String getId() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getId'");
+    }
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+    }
+
+    @Override
+    public String getDescription() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDescription'");
+    }
+
+    @Override
+    public CardType getType() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getType'");
+    }
+
+    @Override
+    public Map<String, Object> getPayload() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPayload'");
+    }
+
+    @Override
+    public boolean isKeepUntilUsed() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isKeepUntilUsed'");
+    }
+    
+}
