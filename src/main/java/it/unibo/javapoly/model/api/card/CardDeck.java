af75cdba18761dbdb9208383ed13623ecc494b96
Francesco Caravita <francesco.caravita2@studio.unibo.it>
2026-02-05 16:24:59 +0100

    Created CardDeck Interface

---
+++ src/main/java/it/unibo/javapoly/model/api/card/CardDeck.java
@@ -0,0 +1,9 @@
+package it.unibo.javapoly.model.api.card;
+
+/**
+ * Represents a deck of game cards.
+ * Provides operations to draw, discard, shuffle cards, and check if the deck is empty.
+ */
+public interface CardDeck {
+
+}
