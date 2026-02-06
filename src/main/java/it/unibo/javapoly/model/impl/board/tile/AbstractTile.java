80894d16f31ae8e69884c955d80f8289c130bec9
Francesco Caravita <francesco.caravita2@studio.unibo.it>
2026-02-06 22:18:45 +0100

    added @JsonSubTypes()

--- src/main/java/it/unibo/javapoly/model/impl/board/tile/AbstractTile.java
+++ src/main/java/it/unibo/javapoly/model/impl/board/tile/AbstractTile.java
@@ -1,5 +1,8 @@
 package it.unibo.javapoly.model.impl.board.tile;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 import it.unibo.javapoly.model.api.board.Tile;
 import it.unibo.javapoly.model.api.board.TileType;
 
@@ -9,6 +12,18 @@ import it.unibo.javapoly.model.api.board.TileType;
  * <p>
  * This class provides common state and behavior for all tile implementations.
  */
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = StartTile.class, name = "start"),
+    @JsonSubTypes.Type(value = LandPropertyTile.class, name = "land_property"),
+    @JsonSubTypes.Type(value = StationPropertyTile.class, name = "station_property"),
+    @JsonSubTypes.Type(value = UtilityPropertyTile.class, name = "utility_property"),
+    @JsonSubTypes.Type(value = TaxTile.class, name = "tax"),
+    @JsonSubTypes.Type(value = JailTile.class, name = "jail"),
+    @JsonSubTypes.Type(value = FreeParkingTile.class, name = "free_parking"),
+    @JsonSubTypes.Type(value = GoToJailTile.class, name = "go_to_jail"),
+    @JsonSubTypes.Type(value = UnexpectedTile.class, name = "unexpected")
+})
 public abstract class AbstractTile implements Tile {
 
     private final int position;
