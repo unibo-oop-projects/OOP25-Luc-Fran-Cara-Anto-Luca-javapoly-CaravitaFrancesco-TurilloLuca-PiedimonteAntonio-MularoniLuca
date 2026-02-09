edbbac74a935909da16ac7958b377e4c0a525b01
Luca Mularoni <luca.mularoni@studio.unibo.it>
2026-02-09 15:40:13 +0100

    Implement in part EconomyController. -Add methods depositToPlayer, withdrawFromPlayer, purchaseProperty, PurchaseHouse, sellHouse, sellProperty, payRent. -Remove getPropertyManager method.

--- src/main/java/it/unibo/javapoly/controller/api/EconomyController.java
+++ src/main/java/it/unibo/javapoly/controller/api/EconomyController.java
@@ -1,8 +1,8 @@
 package it.unibo.javapoly.controller.api;
 
-import it.unibo.javapoly.model.api.economy.Bank;
-import it.unibo.javapoly.model.api.economy.PropertyManager;
+import it.unibo.javapoly.model.api.Player;
 import it.unibo.javapoly.model.api.economy.Transaction;
+import it.unibo.javapoly.model.api.property.Property;
 
 import java.util.List;
 
@@ -12,24 +12,77 @@ import java.util.List;
 public interface EconomyController {
 
     /**
-     * Gets the {@link Bank} instance used for monetary operations.
-     *
-     * @return the {@link Bank} instance.
-     */
-    Bank getBank();
-
-    /**
-     * Gets the {@link PropertyManager} instance used for property operations.
-     *
-     * @return {@link PropertyManager} instance.
-     */
-    PropertyManager getPropertyManager();
-
-    /**
      * Gets the complete transaction history.
      *
      * @return list of all transaction.
      */
     List<Transaction> getTransactions();
 
+    /**
+     * Deposits money to a player from bank.
+     *
+     * @param player the player receiving money.
+     * @param amount the amount to deposit.
+     * @throws NullPointerException if {@code player} is {@code null}.
+     * @throws IllegalArgumentException if {@code amount} <= 0.
+     */
+    void depositToPlayer(Player player, int amount);
+
+    /**
+     * Withdraws money from a player to the bank.
+     *
+     * @param player the player from whom to withdraw.
+     * @param amount the amount to withdraw.
+     * @return {@code true} if successful, {@code false} if insufficient funds (bankruptcy)
+     * @throws NullPointerException if {@code player} is {@code null}.
+     * @throws IllegalArgumentException if {@code amount} <= 0.
+     */
+    boolean withdrawFromPlayer(Player player, int amount);
+
+    /**
+     * Transfer property ownership from bank to player.
+     *
+     * @param buyer future owner of the property.
+     * @param property to buy.
+     * @return {@code true} if successful, {@code false}.
+     */
+    boolean purchaseProperty(Player buyer, Property property);
+
+    /**
+     * Builds a house on a property.
+     *
+     * @param owner of the property.
+     * @param property where to build house.
+     * @return {@code true} if successful, {@code false} otherwise.
+     */
+    boolean purchaseHouse(Player owner, Property property);
+
+    /**
+     * Sell a house from a property to bank.
+     *
+     * @param owner of the property.
+     * @param property where to sell house.
+     * @return {@code true} if successful, {@code false} if insufficient funds (bankruptcy).
+     */
+    boolean sellHouse(Player owner, Property property);
+
+    /**
+     * Sell a property to bank.
+     *
+     * @param owner ex-owner of the property.
+     * @param property property to sell.
+     * @return {@code true} if successful, {@code false} otherwise.
+     */
+    boolean sellProperty(Player owner, Property property);
+
+    /**
+     * Pay rent to payee.
+     *
+     * @param payer the player who make payment.
+     * @param payee tha player who collect payment.
+     * @param property property where is the payer (owned by payee).
+     * @param diceRoll the dice roll used to calculate rent (especially for utilities).
+     * @return {@code true} if successful, {@code false} if insufficient funds (bankruptcy).
+     */
+    boolean payRent(Player payer, Player payee, Property property, int diceRoll);
 }
--- src/main/java/it/unibo/javapoly/controller/impl/EconomyControllerImpl.java
+++ src/main/java/it/unibo/javapoly/controller/impl/EconomyControllerImpl.java
@@ -1,50 +1,45 @@
 package it.unibo.javapoly.controller.impl;
 
 import it.unibo.javapoly.controller.api.EconomyController;
+import it.unibo.javapoly.controller.api.PropertyController;
+import it.unibo.javapoly.model.api.Player;
 import it.unibo.javapoly.model.api.economy.Bank;
-import it.unibo.javapoly.model.api.economy.PropertyManager;
 import it.unibo.javapoly.model.api.economy.Transaction;
 import it.unibo.javapoly.model.api.property.Property;
 import it.unibo.javapoly.model.impl.economy.BankImpl;
-import it.unibo.javapoly.model.impl.economy.PropertyManagerImpl;
+import it.unibo.javapoly.utils.ValidationUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import static it.unibo.javapoly.model.api.economy.TransactionType.DEPOSIT_FROM_BANK;
+import static it.unibo.javapoly.model.api.economy.TransactionType.WITHDRAW_TO_BANK;
+import static it.unibo.javapoly.model.api.economy.TransactionType.BUY_HOUSE;
+import static it.unibo.javapoly.model.api.economy.TransactionType.PAY_RENT;
+import static it.unibo.javapoly.model.api.economy.TransactionType.SELL_HOUSE;
+import static it.unibo.javapoly.model.api.economy.TransactionType.SELL_PROPERTY;
+import static it.unibo.javapoly.model.api.economy.TransactionType.BUY_PROPERTY;
 
 /**
  * Implementation of the EconomyController interface.
  */
-public class EconomyControllerImpl implements EconomyController {
+public final class EconomyControllerImpl implements EconomyController {
 
     private final Bank bank;
-    private final PropertyManager propertyManager;
-    private final List<Transaction> transactionHistory;
+    private final PropertyController propertyController;
+    private final List<Transaction> transactionHistory = new ArrayList<>();
+    private int nextTransactionId = 1;
 
     /**
      * Creates an EconomyController with the given list of properties.
      *
-     * @param properties the list of all properties in the game
+     * @param propertyController property controller.
      */
-    public EconomyControllerImpl(final List<Property> properties) {
+    public EconomyControllerImpl(final PropertyController propertyController) {
         this.bank = new BankImpl();
-        this.propertyManager = new PropertyManagerImpl(properties);
-        this.transactionHistory = new ArrayList<>();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Bank getBank() {
-        return this.bank;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public PropertyManager getPropertyManager() {
-        return this.propertyManager;
+        this.propertyController = propertyController;
     }
 
     /**
@@ -53,5 +48,150 @@ public class EconomyControllerImpl implements EconomyController {
     @Override
     public List<Transaction> getTransactions() {
         return new ArrayList<>(this.transactionHistory);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void depositToPlayer(final Player player, final int amount) {
+        this.bank.deposit(player, amount);
+        recordTransaction(new Transaction(this.nextTransactionId,
+                    DEPOSIT_FROM_BANK,
+                    Optional.empty(),
+                    Optional.of(player.getName()),
+                    Optional.empty(),
+                    amount));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean withdrawFromPlayer(final Player player, final int amount) {
+        if (this.bank.withdraw(player, amount)) {
+            recordTransaction(new Transaction(this.nextTransactionId,
+                    WITHDRAW_TO_BANK,
+                    Optional.of(player.getName()),
+                    Optional.empty(),
+                    Optional.empty(),
+                    amount));
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean payRent(final Player payer, final Player payee, final Property property, final int diceRoll) {
+        final int rent = this.propertyController.payRent(payer, property.getId(), diceRoll);
+        if (this.bank.transferFunds(payer, payee, rent)) {
+            recordTransaction(new Transaction(this.nextTransactionId,
+                    PAY_RENT,
+                    Optional.of(payer.getName()),
+                    Optional.of(payee.getName()),
+                    Optional.of(property.getId()),
+                    rent));
+            return true;
+        }
+        //TODO bankrupt
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean purchaseProperty(final Player buyer, final Property property) {
+        final int price = property.getPurchasePrice();
+        if (this.bank.withdraw(buyer, price)) {
+            this.propertyController.purchaseProperty(buyer, property.getId());
+            recordTransaction(new Transaction(this.nextTransactionId,
+                    BUY_PROPERTY,
+                    Optional.of(buyer.getName()),
+                    Optional.empty(),
+                    Optional.of(property.getId()),
+                    price));
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean purchaseHouse(final Player owner, final Property property) {
+        final int houseCost = this.propertyController.getHouseCost(property);
+        ValidationUtils.requirePositive(houseCost, "Is not a land");
+        if (this.bank.canAfford(owner, houseCost)) {
+            if (this.propertyController.buildHouse(owner, property.getId())) {
+                this.bank.withdraw(owner, houseCost); //it cant fail because with the first if we see that player have money
+                recordTransaction(new Transaction(this.nextTransactionId,
+                        BUY_HOUSE,
+                        Optional.of(owner.getName()),
+                        Optional.empty(),
+                        Optional.of(property.getId()),
+                        houseCost));
+                return true;
+            }
+            throw new IllegalStateException("You don't own all the properties of the same color/ te house are not homogeneous");
+        }
+        //TODO bankrupt
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean sellHouse(final Player owner, final Property property) {
+        final int price = this.propertyController.getHouseCost(property) / 2;
+        if (this.propertyController.destroydHouse(owner, property.getId())) {
+            this.bank.deposit(owner, price);
+            recordTransaction(new Transaction(this.nextTransactionId,
+                    SELL_HOUSE,
+                    Optional.empty(),
+                    Optional.of(owner.getName()),
+                    Optional.of(property.getId()),
+                    price));
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean sellProperty(final Player owner, final Property property) {
+        Objects.requireNonNull(owner, "owner cannot be null");
+        ValidationUtils.requireNonBlank(owner.getName(), "owner cannot be the bank");
+        Objects.requireNonNull(property, "property cannot be null");
+        if (!property.getState().getOwnerId().equals(owner.getName())) {
+            return false;
+        }
+        final int price = property.getPurchasePrice() / 2;
+        this.propertyController.returnPropertyToBank(property);
+        this.bank.deposit(owner, price);
+        recordTransaction(new Transaction(this.nextTransactionId,
+                    SELL_PROPERTY,
+                    Optional.empty(),
+                    Optional.of(owner.getName()),
+                    Optional.of(property.getId()),
+                    price));
+        return true;
+    }
+
+    /**
+     * Records a transaction in the history.
+     *
+     * @param transaction the transaction to record.
+     */
+    private void recordTransaction(final Transaction transaction) {
+        this.transactionHistory.add(transaction);
+        this.nextTransactionId++;
     }
 }
