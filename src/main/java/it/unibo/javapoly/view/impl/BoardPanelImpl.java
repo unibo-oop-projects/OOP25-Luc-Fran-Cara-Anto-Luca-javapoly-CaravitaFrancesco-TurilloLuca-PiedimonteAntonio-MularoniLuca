7c2a74053032a2171e297450a605d5e7855a571e
Francesco Caravita <francesco.caravita2@studio.unibo.it>
2026-02-13 22:57:29 +0100

    fixed

--- src/main/java/it/unibo/javapoly/view/impl/BoardPanelImpl.java
+++ src/main/java/it/unibo/javapoly/view/impl/BoardPanelImpl.java
@@ -139,8 +139,8 @@ public final class BoardPanelImpl implements BoardPanel {
         }
 
         if (img == null || img.isError()) {
-            final String imageName = p.getTokenType().toString().toUpperCase() + ".png";
-            final var stream = getClass().getResourceAsStream("/images/tokens/" + imageName);
+            final String imageName = p.getTokenType().toString().toUpperCase(Locale.ROOT) + ".png";
+            final var stream = BoardPanelImpl.class.getResourceAsStream("/images/tokens/" + imageName);
             if (stream != null) {
                 img = new Image(stream);
             }
